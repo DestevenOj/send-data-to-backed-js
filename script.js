54ee001ff35d21c9a002127ab6751a90a12d5c8a
Stephen Ochieng <stephenojwang1040@gmail.com>
2019-10-14 21:11:25 +0300

    send data to backed with post request

--- script.js
+++ script.js
@@ -3,40 +3,36 @@ const titleInput = document.getElementById('title');
 const contentInput = document.getElementById('content');
 const submitButton = document.getElementById('submit-button');
 
-const api = 'https://us-central1-open-classrooms-js-for-the-web.cloudfunctions.net/widgets';
+const url = 'https://us-central1-open-classrooms-js-for-the-web.cloudfunctions.net/widgets';
 
 // Get DOM elements for showing response
 const responseMessage = document.getElementById('response-message');
 const responseTitle = document.getElementById('response-title');
+const responseId = document.getElementById('response-id');
 const responseContent = document.getElementById('response-content');
 
-submitButton.addEventListener($click) => {
-  $click.preventDefault();
+submitButton.addEventListener('click', ($event) => {
+  $event.preventDefault();
   const post = {
     title: titleInput.value,
     content: contentInput.value
   };
-};
+});
 
 function makeRequest(data) {
- return new Promise((resolve, reject) => {
-   let request = XMLHttpRequest();
-   request.open('POST', api + '/create-post')
-   
-   request.onreadystatechange = () => {
-     if (request.readyState === 4) {
-       if (request.status === 201) {
-         resolve(JSON.parse(request.response));
-       } else {
-         reject(JSON.parse(request.response));
-       }
-     }
-   };
-   request.setRequestHeader('Content-Type', 'application/json');
-   request.send(JSON.stringify(data));
- }); 
-};
-
-
-
-
+  return new Promise((resolve, reject) => {
+    let request = new XMLHttpRequest();
+    request.open('POST', url + '/create-post');
+    request.onreadystatechange = () => {
+      if (request.readyState === 4) {
+        if (request.status === 201) {
+          resolve(JSON.parse(request.response));
+        } else {
+          reject(JSON.parse(request.response));
+        }
+      }
+    };
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify(data));
+  });
+}
